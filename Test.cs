7624835593874a3847885d9e46474e6993579249
Sohaib <@SohaibHN>
2017-12-24 11:48:58 +0000

    Test Local!

---
+++ Test.cs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8" ?>
+<configuration>
+    <configSections>
+    </configSections>
+    <connectionStrings>
+        <add name="SE_A_Assignment2.Properties.Settings.BugTrackerConnectionString"
+            connectionString="Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\BugTracker.mdf;Integrated Security=True;Connect Timeout=30"
+            providerName="System.Data.SqlClient" />
+      <add name="BugTrackerDB"
+     connectionString="Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\database\BugTracker.mdf;Integrated Security=True;Connect Timeout=30"
+     providerName="System.Data.SqlClient" />
+        <add name="SE_A_Assignment2.Properties.Settings.BugTrackerConnectionString1"
+            connectionString="Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\database\BugTracker.mdf;Integrated Security=True"
+            providerName="System.Data.SqlClient" />
+    </connectionStrings>
+    <startup> 
+        <supportedRuntime version="v4.0" sku=".NETFramework,Version=v4.6.1" />
+    </startup>
+</configuration>
